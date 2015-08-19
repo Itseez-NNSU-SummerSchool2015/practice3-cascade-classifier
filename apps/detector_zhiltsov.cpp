319e6818226e6276963098e41650faab67e51b82
unknown <SS2015@ws-2k-114-04n.ccam.cmc.unn.net>
2015-08-19 17:46:24 +0300

    Implementation for image added.

--- apps/detector_zhiltsov.cpp
+++ apps/detector_zhiltsov.cpp
@@ -5,6 +5,7 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2\imgproc\imgproc.hpp"
 
 using namespace std;
 using namespace cv;
@@ -12,6 +13,8 @@ using namespace cv;
 const char* params =
      "{ h | help     | false | print usage                                   }"
      "{   | detector |       | XML file with a cascade detector              }"
+	 "{   | d2       |       | Additional detector                           }"
+	 "{   | d3       |       | Additional detector                           }"
      "{   | image    |       | image to detect objects on                    }"
      "{   | video    |       | video file to detect on                       }"
      "{   | camera   | false | whether to detect on video stream from camera }";
@@ -32,6 +35,18 @@ const Scalar green(0, 255, 0);
 const Scalar blue(255, 0, 0);
 const Scalar colors[] = {red, green, blue};
 
+void detect(const Mat& src, Mat& dst, vector<CascadeClassifier>& detector) {
+	//const Size kSize(5, 5);
+	//GaussianBlur(src, dst, kSize, 3); 
+	dst = src.clone();
+
+	for (auto it = detector.begin(), iend = detector.end(); it != iend; ++it) {
+		vector<Rect> objects;
+		(*it).detectMultiScale(dst, objects);
+		drawDetections(objects, colors[it - detector.cbegin()], dst);
+	}	
+}
+
 int main(int argc, char** argv)
 {
     // Parse command line arguments.
@@ -44,33 +59,27 @@ int main(int argc, char** argv)
     }
 
     string detector_file = parser.get<string>("detector");
+	string d2_file = parser.get<string>("d2");
+	string d3_file = parser.get<string>("d3");
     CV_Assert(!detector_file.empty());
     string image_file = parser.get<string>("image");
     string video_file = parser.get<string>("video");
     bool use_camera = parser.get<bool>("camera");
 
-    CascadeClassifier detector;
-	if (!detector.load(detector_file)) 
-	{
-		cerr << "Failed to load detector." << endl;
-		return 1;
+    vector<CascadeClassifier> detector;
+	detector.emplace_back(detector_file);
+	if (d2_file.empty() == false) {
+		detector.emplace_back(d2_file);
+	}
+	if (d3_file.empty() == false) {
+		detector.emplace_back(d3_file);
 	}
 
     if (!image_file.empty())
     {
 		Mat img = imread(image_file);
-		Mat out = img.clone();
-
-		vector<Rect> objects;
-        detector.detectMultiScale(img, objects);
-
-		cout << "Found detections: " << endl;
-		for (auto it = objects.cbegin(), iend = objects.cend(); it != iend; ++it) {
-			cout << *it << endl;
-		}
-
-		drawDetections(objects, red, out);
-
+		Mat out;
+		detect(img, out, detector);
 		imshow("Detected frames", out);
 		waitKey();
     }
@@ -83,19 +92,12 @@ int main(int argc, char** argv)
 		vector<Rect> objects;
 
 		video >> img;
-		video >> img; //bugfix on Win
 		while (img.empty() == false) {
 			if (waitKey(30) >= 0) { break; }
-
-			out = img.clone();
-
-			detector.detectMultiScale(img, objects);
-			drawDetections(objects, red, out);
+			detect(img, out, detector);
 			imshow("Detected frames", out);
-
 			video >> img;
 		}
-		waitKey();
 
 		video.release();
     }
@@ -111,17 +113,10 @@ int main(int argc, char** argv)
 		video >> img; //bugfix on Win
 		while (img.empty() == false) {
 			if (waitKey(30) >= 0) { break; }
-
-			out = img.clone();
-
-			detector.detectMultiScale(img, objects);
-			drawDetections(objects, red, out);
-
+			detect(img, out, detector);
 			imshow("Detected frames", out);
-
 			video >> img;
 		}
-		// waitKey();
 
 		video.release();
     }
